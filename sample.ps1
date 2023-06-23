e38ca668e6099f41215cc55bc17cb72194330630
coskunarif <78119659+coskunarif@users.noreply.github.com>
2023-06-23 02:32:14 -0700

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,28 +1,12 @@
-# Set the local folder path where the PDF files are stored
-$folderPath = "C:\temp\test"
+$processes = Get-Process
 
-# Initialize variables for storing the corrupted file names and count
-$corruptedFileNames = @()
-$corruptedFileCount = 0
+foreach ($process in $processes)
+{
+    $cpuUsage = ($process.CPU * 100) / (Get-WmiObject -Class Win32_Processor).MaxClockSpeed
 
-# Get a list of all PDF files in the folder
-$pdfFiles = Get-ChildItem -Path $folderPath -Filter "*.pdf"
-
-# Loop through each PDF file
-foreach ($pdfFile in $pdfFiles) {
-  # Try to read the PDF file using the Adobe PDF library
-  Try {
-    $pdfDocument = New-Object -ComObject AcroExch.PDDoc
-    $pdfDocument.Open($pdfFile.FullName)
-  }
-  Catch {
-    # If the PDF file is corrupted, add its name to the list of corrupted files
-    # and increment the count
-    $corruptedFileNames += $pdfFile.Name
-    $corruptedFileCount++
-  }
+    if ($cpuUsage -gt 50)
+    {
+        # If so, print the process name and its CPU usage
+        Write-Host ("Process: " + $process.Name + ", CPU Usage: " + $cpuUsage + "%")
+    }
 }
-
-# Output the names of the corrupted files and the total count
-Write-Output "Corrupted files: $corruptedFileNames"
-Write-Output "Total count: $corruptedFileCount"
